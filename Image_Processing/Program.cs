6fc8c6ce6ec43834c80519607d4abb4d52f3c13f
khampaserth xayyavong <khampaserthxayyavong@hotmail.com>
2021-11-09 21:19:36 +0700

    Update 9/11/2021

--- Image_Processing/Program.cs
+++ Image_Processing/Program.cs
@@ -19,13 +19,13 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new SetpixelForm());
-            //Application.Run(new GetPixel());
-            //Application.Run(new Get_Setpixel());
-            //Application.Run(new GrayCV());
-            //Application.Run(new GrayScale_Black_and_White_and_Invert());
-            //Application.Run(new Emgu_Begining());
-            //Application.Run(new Image_HistogramBox());
+            Application.Run(new SetpixelForm());
+            Application.Run(new GetPixel());
+            Application.Run(new Get_Setpixel());
+            Application.Run(new GrayCV());
+            Application.Run(new GrayScale_Black_and_White_and_Invert());
+            Application.Run(new Emgu_Begining());
+            Application.Run(new Image_HistogramBox());
             Application.Run(new Image_Processing_to_Black_and_White());
         }
     }
